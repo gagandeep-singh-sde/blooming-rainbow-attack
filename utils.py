3c2784ebbefa182e89703a2bfa27d8bd4f4208f4
Gagandeep Singh <gagandeep.singh.sde@gmail.com>
2024-11-29 02:03:52 -0500

    fix backslash escape in validator

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ def is_valid_password(password):
     has_upper = any(c.isupper() for c in password)
     has_lower = any(c.islower() for c in password)
     has_digit = any(c.isdigit() for c in password)
-    has_special = any(c in "~!@#$%^&*()_+{}|:\"<>?`-=[]\;',./ " for c in password)
+    has_special = any(c in "~!@#$%^&*()_+{}|:\"<>?`-=[]\\;',./ " for c in password)
     return has_upper and has_lower and has_digit and has_special
 
 
