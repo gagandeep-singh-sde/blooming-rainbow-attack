55b3f402348ca61d74d1d76083adc0418386d133
Gagandeep Singh <gagandeep.singh.sde@gmail.com>
2024-12-07 15:27:05 -0500

    add concurrency for password cracking

--- bloomy-rainbow-attack.py
+++ bloomy-rainbow-attack.py
@@ -1,9 +1,9 @@
 import bcrypt
-from rbloom import Bloom
 import itertools
-from utils import get_character_set, is_valid_password, consistent_hash
 from multiprocessing import Pool
 import time
+from rbloom import Bloom
+from utils import get_character_set, is_valid_password, consistent_hash
 from constants import (
     PASSWORD_LENGTH,
     PROCESSOR_CORES,
@@ -39,34 +39,60 @@ def find_hash_in_bloom_filters(given_hash, num_bloom_filters):
     return found_indices
 
 
+def process_chunk(start, end, given_hash):
+    passwords = []
+    batch_size = BATCH_SIZE // 10
+    for batch_start in range(start, end, batch_size):
+        batch_end = min(batch_start + batch_size, end)
+        for i, password_tuple in enumerate(
+            itertools.product(characters, repeat=PASSWORD_LENGTH)
+        ):
+            if i < batch_start:
+                continue
+            if i >= batch_end:
+                break
+            password = "".join(password_tuple)
+            if is_valid_password(password):
+                password_bytes = password.encode("utf-8")
+                hashed = bcrypt.hashpw(password_bytes, fixed_salt).decode("utf-8")
+                if hashed == given_hash:
+                    passwords.append(password)
+    return passwords
+
+
 def get_passwords_for_hash(given_hash):
     total_combinations = len(characters) ** PASSWORD_LENGTH
     num_bloom_filters = (total_combinations + BATCH_SIZE - 1) // BATCH_SIZE
     found_indices = find_hash_in_bloom_filters(given_hash, num_bloom_filters)
     passwords = []
     if found_indices:
-        for index in found_indices:
-            start = index * BATCH_SIZE
-            end = min((index + 1) * BATCH_SIZE, total_combinations)
-            for i, password_tuple in enumerate(
-                itertools.product(characters, repeat=PASSWORD_LENGTH)
-            ):
-                if i < start:
-                    continue
-                if i >= end:
-                    break
-                password = "".join(password_tuple)
-                if is_valid_password(password):
-                    password_bytes = password.encode("utf-8")
-                    hashed = bcrypt.hashpw(password_bytes, fixed_salt).decode("utf-8")
-                    if hashed == given_hash:
-                        passwords.append(password)
+        ranges = [
+            (
+                index * BATCH_SIZE,
+                min((index + 1) * BATCH_SIZE, total_combinations),
+                given_hash,
+            )
+            for index in found_indices
+        ]
+        # Flatten the ranges to distribute the work more evenly
+        flat_ranges = []
+        for start, end, given_hash in ranges:
+            chunk_size = (end - start) // 10
+            for i in range(10):
+                chunk_start = start + i * chunk_size
+                chunk_end = start + (i + 1) * chunk_size if i < 9 else end
+                flat_ranges.append((chunk_start, chunk_end, given_hash))
+
+        with Pool(PROCESSOR_CORES) as pool:
+            results = pool.starmap(process_chunk, flat_ranges)
+            for result in results:
+                passwords.extend(result)
     return passwords
 
 
 if __name__ == "__main__":
+    given_hash = input("Enter the hash value: ")
     start_time = time.time()
-    given_hash = input("Enter the hash value: ")  # 5C
     passwords = get_passwords_for_hash(given_hash)
     if passwords:
         for password in passwords:
